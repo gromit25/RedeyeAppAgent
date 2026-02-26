b95469f2ed0c7fe1e0f336d387b8cebc5c072ac5
gromit25 <gromit25@hanmail.net>
2026-02-26 20:46:30 +0900

    주석 추가

--- src/main/java/com/redeye/agent/domain/jdbc/acquisitor/advice/ConnectionAdvice.java
+++ src/main/java/com/redeye/agent/domain/jdbc/acquisitor/advice/ConnectionAdvice.java
@@ -5,19 +5,19 @@ import java.lang.reflect.Method;
 import net.bytebuddy.asm.Advice;
 
 /**
- * 
+ * Connection 어드바이스 클래스
  * 
  * @author jmsohn
  */
 public class ConnectionAdvice {
 	
 	
-	/** */
+	/** PreparedStatement 생성시 sql 문 저장용 변수 */
 	public static ThreadLocal<String> sql = ThreadLocal.withInitial(() -> "");
 	
 	
 	/**
-	 * 
+	 * PreparedStatement 생성시 sql 문 반환
 	 * 
 	 * @return
 	 */
@@ -26,12 +26,15 @@ public class ConnectionAdvice {
 	}
 	
 	/**
-	 * 
+	 * Connection.prepareStatement 어드바이스 클래스
 	 */
 	public static class prepareStatement {
-		
+
 		/**
+		 * prepareStatement 호출 전 콜백 메소드
 		 * 
+		 * @param method 호출된 실제 메소드
+		 * @param args 메소드의 아규먼트
 		 */
 		@Advice.OnMethodEnter
 		public static void onEnter(@Advice.Origin Method method, @Advice.AllArguments Object[] args) {
@@ -46,12 +49,12 @@ public class ConnectionAdvice {
 	}
 	
 	/**
-	 * 
+	 * Connection.commit 어드바이스 클래스
 	 */
 	public static class commit {
 		
 		/**
-		 * 
+		 * commit 호출 후 콜백 메소드
 		 */
 		@Advice.OnMethodExit
 		public static void onExit() {
@@ -60,12 +63,12 @@ public class ConnectionAdvice {
 	}
 
 	/**
-	 * 
+	 * Connection.rollback 어드바이스 클래스
 	 */
 	public static class rollback {
 
 		/**
-		 * 
+		 * rollback 호출 후 콜백 메소드
 		 */
 		@Advice.OnMethodExit
 		public static void onExit() {
@@ -74,12 +77,12 @@ public class ConnectionAdvice {
 	}
 
 	/**
-	 * 
+	 * Connection.close 어드바이스 클래스
 	 */
 	public static class close {
 		
 		/**
-		 * 
+		 * close 호출 후 콜백 메소드
 		 */
 		@Advice.OnMethodExit
 		public static void onExit() {
