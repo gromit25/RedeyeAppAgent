614cca0f74ad67f33e99c4b71228e94bcd26c91f
jung mo <gromit25@hanmail.net>
2026-05-13 16:28:03 +0900

    Create Loader.java

---
+++ src/main/java/com/redeye/agent/loader/Loader.java
@@ -0,0 +1,52 @@
+package com.redeye.agent.loader;
+
+/**
+ *
+ *
+ * @author jmsohn
+ */
+public class Loader {
+
+  /**
+   *
+   */
+  public static void start(List<Context> contextList) {
+
+    // 컨텍스트가 없는 경우 반환
+    if(contextList == null || contextList.size() == 0) {
+      return;
+    }
+    
+		// ------------------------
+		// 로더 기동 여부 확인
+		String useLoader = Config.LOADER_YN.value;
+		if("Y".equalsIgnoreCase(useLoader) == false) {
+			LogUtil.log("metrics api loader is disabled.");
+			return;
+		}
+		
+		// ------------------------
+		// 로더 기동을 위한 옵션 획득
+		
+		// 호출할 API의 기준 패스 획득
+		String basePath = Config.LOADER_API_SERVER.value;
+		
+		// API 호출 스케쥴 획득
+		String schedule = Config.LOADER_SCHEDULE.value;
+		
+		// ------------------------
+		// API 호출 로더 목록 설정
+		List<APILoader> loaderList = new ArrayList<>();
+		
+		for(Context context: contextList) {
+			loaderList.addAll(context.getAPILoaderList());
+		}
+		
+		// ------------------------
+		// API 호출 로더 생성 및 기동
+		loader = new APILoaderCronJob(basePath, schedule, loaderList);
+		loader.start();
+		
+		LogUtil.log("metrics api loader is started.");
+  }
+}
