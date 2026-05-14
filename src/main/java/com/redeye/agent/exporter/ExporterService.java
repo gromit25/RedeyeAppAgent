7c282ed14cb2503134e611e2de720136e5f06142
jung mo <gromit25@hanmail.net>
2026-05-14 10:53:44 +0900

    Update ExporterService.java

--- src/main/java/com/redeye/agent/exporter/ExporterService.java
+++ src/main/java/com/redeye/agent/exporter/ExporterService.java
@@ -1,20 +1,21 @@
 package com.redeye.agent.exporter;
 
 /**
- *
+ * Http Exporter Service 클래스<br>
+ * 외부에서 Http를 통해 성능 정보를 접근할 수 있는 서비스
  *
  * @author jmsohn
  */
 public class ExporterService {
 
 	/**
+	 * Http Exporter Service 기동
 	 *
-	 *
-	 * @param contextList
+	 * @param contextList 컨텍스트 목록
 	 */
 	public static void start(List<Context> contextList) {
 
-		//
+		// 컨텍스트가 없는 경우 반환
 		if(contextList == null || contextList.size() == 0) {
 			return;
 		}
